b84dee4600d2c4d79379163c1e261d15f1dabb67
Jason Nathaniel <jasonnathaniel1152@gmail.com>
2021-06-15 22:42:32 +0800

    [Home Page] Added 'follow me' content

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react";
 import clsx from "clsx";
-import { makeStyles, Toolbar } from "@material-ui/core";
+import { makeStyles, Toolbar, Divider } from "@material-ui/core";
 
 // import colors
 import { colors, iconPillColor } from "../common";
 
 // import components
-import { PillContainer, HeaderText, Text } from "../common";
+import {
+  PillContainer,
+  HeaderText,
+  Text,
+  StyledCard,
+  StyledButton,
+} from "../common";
 import HomeContent from "./content";
 
 export default function HomePage(props) {
   const homePageClasses = homePageStyles();
+  const aboutMeClasses = aboutMeStyles();
+  const cardClasses = cardStyles();
   const [startAnimation, setStartAnimation] = useState(false);
 
   useEffect(() => {
@@ -21,118 +29,169 @@ export default function HomePage(props) {
   }, []);
 
   return (
-    <div className={homePageClasses.root}>
-      <Toolbar />
-      <header className={homePageClasses.header}>
-        <HeaderText
-          className={homePageClasses.headerTitle}
-          style={{ fontWeight: "800" }}
+    <React.Fragment>
+      <div className={homePageClasses.root}>
+        <Toolbar />
+        <header className={homePageClasses.header}>
+          <HeaderText
+            className={homePageClasses.headerTitle}
+            style={{ fontWeight: "800" }}
+          >
+            Oh, Hello There!
+          </HeaderText>
+          <div
+            className={homePageClasses.headerTitleUnderline1}
+            style={{ width: startAnimation ? "60%" : 0 }}
+          />
+          <div
+            className={homePageClasses.headerTitleUnderline2}
+            style={{ width: startAnimation ? "80%" : 0 }}
+          />
+        </header>
+        <div
+          className={homePageClasses.body}
+          style={{ opacity: startAnimation ? 1 : 0 }}
         >
-          Oh, Hello There!
-        </HeaderText>
-        <div
-          className={homePageClasses.headerTitleUnderline1}
-          style={{ width: startAnimation ? "60%" : 0 }}
-        />
-        <div
-          className={homePageClasses.headerTitleUnderline2}
-          style={{ width: startAnimation ? "80%" : 0 }}
-        />
-      </header>
-      <div
-        className={homePageClasses.body}
-        style={{ opacity: startAnimation ? 1 : 0 }}
-      >
-        <section className={homePageClasses.personalContentContainer}>
-          <div className={homePageClasses.personalContentBackground} />
-          <div className={homePageClasses.personalContent1}>
-            <header className={homePageClasses.personalContentHeader}>
-              <HeaderText className={clsx(homePageClasses.textColor)}>
-                About Me
-              </HeaderText>
-              <div style={{ display: "flex" }}>
-                <div className={homePageClasses.animatedCircle} />
-                <div
-                  className={homePageClasses.animatedCircle}
-                  style={{ animationDelay: "0.3s" }}
+          <section className={homePageClasses.personalContentContainer}>
+            <div className={homePageClasses.personalContentBackground} />
+            <div className={homePageClasses.personalContent1}>
+              <header className={homePageClasses.personalContentHeader}>
+                <HeaderText className={clsx(homePageClasses.textColor)}>
+                  About Me
+                </HeaderText>
+                <div style={{ display: "flex" }}>
+                  <div className={homePageClasses.animatedCircle} />
+                  <div
+                    className={homePageClasses.animatedCircle}
+                    style={{ animationDelay: "0.3s" }}
+                  />
+                  <div
+                    className={homePageClasses.animatedCircle}
+                    style={{ animationDelay: "0.6s" }}
+                  />
+                </div>
+              </header>
+              <div className={homePageClasses.personalContentText}>
+                <Text
+                  style={{ textAlign: "right" }}
+                  className={clsx(homePageClasses.textColor)}
+                >
+                  <li>
+                    I am a <strong>Software Engineer</strong> at heart with a
+                    self-driven motivation to learn, share, and embrace new
+                    emerging technologies.
+                  </li>
+                </Text>
+                <Text
+                  style={{ textAlign: "right" }}
+                  className={clsx(homePageClasses.textColor)}
+                >
+                  <li>
+                    4th year student, majoring in{" "}
+                    <b>Electrical and Electronic Engineering</b> specializing in{" "}
+                    <b>Computer Engineering</b> at Nanyang Technological
+                    University, Singapore.
+                  </li>
+                </Text>
+              </div>
+            </div>
+            <div className={homePageClasses.personalContent2}>
+              <header className={homePageClasses.personalContentHeader}>
+                <HeaderText className={clsx(homePageClasses.textColor)}>
+                  Expertise
+                </HeaderText>
+                <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+                  <div
+                    className={homePageClasses.animatedCircle}
+                    style={{ animationDelay: "0.9s" }}
+                  />
+                  <div
+                    className={homePageClasses.animatedCircle}
+                    style={{ animationDelay: "1.3s" }}
+                  />
+                  <div
+                    className={homePageClasses.animatedCircle}
+                    style={{ animationDelay: "1.6s" }}
+                  />
+                </div>
+              </header>
+              <div className={homePageClasses.personalContentText}>
+                <PillContainer
+                  text="react"
+                  shadow={true}
+                  backgroundColor={iconPillColor.react.bg}
+                  fontColor={iconPillColor.react.fc}
                 />
-                <div
-                  className={homePageClasses.animatedCircle}
-                  style={{ animationDelay: "0.6s" }}
+                <PillContainer
+                  text="node.js"
+                  shadow={true}
+                  backgroundColor={iconPillColor["node.js"].bg}
+                  fontColor={iconPillColor["node.js"].fc}
+                />
+                <PillContainer
+                  text="express"
+                  shadow={true}
+                  backgroundColor={iconPillColor.express.bg}
+                  fontColor={iconPillColor.express.fc}
+                />
+                <PillContainer
+                  text="mongodb"
+                  shadow={true}
+                  backgroundColor={iconPillColor.mongodb.bg}
+                  fontColor={iconPillColor.mongodb.fc}
                 />
               </div>
-            </header>
-            <div className={homePageClasses.personalContentText}>
-              <Text
-                style={{ textAlign: "right" }}
-                className={clsx(homePageClasses.textColor)}
-              >
-                <li>
-                  I am a <strong>Software Engineer</strong> at heart with a
-                  self-driven motivation to learn, share, and embrace new
-                  emerging technologies.
-                </li>
-              </Text>
-              <Text
-                style={{ textAlign: "right" }}
-                className={clsx(homePageClasses.textColor)}
-              >
-                <li>
-                  4th year student, majoring in{" "}
-                  <b>Electrical and Electronic Engineering</b> specializing in{" "}
-                  <b>Computer Engineering</b> at Nanyang Technological
-                  University, Singapore.
-                </li>
-              </Text>
             </div>
-          </div>
-          <div className={homePageClasses.personalContent2}>
-            <header className={homePageClasses.personalContentHeader}>
-              <HeaderText className={clsx(homePageClasses.textColor)}>
-                Expertise
-              </HeaderText>
-              <div style={{ display: "flex", flexDirection: "row-reverse" }}>
-                <div
-                  className={homePageClasses.animatedCircle}
-                  style={{ animationDelay: "0.9s" }}
-                />
-                <div
-                  className={homePageClasses.animatedCircle}
-                  style={{ animationDelay: "1.3s" }}
-                />
-                <div
-                  className={homePageClasses.animatedCircle}
-                  style={{ animationDelay: "1.6s" }}
-                />
-              </div>
-            </header>
-            <div className={homePageClasses.personalContentText}>
-              <PillContainer
-                text="react"
-                backgroundColor={iconPillColor.react.bg}
-                fontColor={iconPillColor.react.fc}
-              />
-              <PillContainer
-                text="node.js"
-                backgroundColor={iconPillColor["node.js"].bg}
-                fontColor={iconPillColor["node.js"].fc}
-              />
-              <PillContainer
-                text="express"
-                backgroundColor={iconPillColor.express.bg}
-                fontColor={iconPillColor.express.fc}
-              />
-              <PillContainer
-                text="mongodb"
-                backgroundColor={iconPillColor.mongodb.bg}
-                fontColor={iconPillColor.mongodb.fc}
-              />
-            </div>
-          </div>
-        </section>
-        <HomeContent startAnimation={startAnimation} />
+          </section>
+          <HomeContent startAnimation={startAnimation} />
+        </div>
       </div>
-    </div>
+      <div className={aboutMeClasses.root}>
+        <Toolbar />
+        <header className={aboutMeClasses.header}>
+          <HeaderText>Don't Forget to Follow Me!</HeaderText>
+        </header>
+        <Toolbar />
+        <div className={aboutMeClasses.body}>
+          <StyledCard
+            borderRadius={10}
+            className={cardClasses.root}
+            bgColor={colors.gray}
+            width="320px"
+            height="400px"
+          >
+            <header className={cardClasses.header}>
+              <img
+                className={cardClasses.imageHeader}
+                src="github.png"
+                alt="Github"
+              />
+              <HeaderText>Test</HeaderText>
+            </header>
+            <div className={cardClasses.body}>
+              <Text>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+              </Text>
+            </div>
+            <footer className={cardClasses.footer}>
+              <StyledButton>Test</StyledButton>
+            </footer>
+          </StyledCard>
+          <StyledCard
+            className={cardClasses.root}
+            bgColor={colors.gray}
+            width="320px"
+            height="400px"
+          />
+          <StyledCard
+            className={cardClasses.root}
+            bgColor={colors.gray}
+            width="320px"
+            height="400px"
+          />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
@@ -334,5 +393,92 @@ const homePageStyles = makeStyles((theme) => ({
     "0%": { opacity: 0.3 },
     "50%": { opacity: 1 },
     "100%": { opacity: 0.3 },
+  },
+}));
+
+const aboutMeStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    minHeight: "100vh",
+    background:
+      "linear-gradient(107deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)",
+    padding: theme.spacing(2),
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+    boxSizing: "border-box",
+  },
+  header: {
+    width: "50%",
+    fontSize: "32pt",
+    boxSizing: "border-box",
+    overflow: "hidden",
+    position: "relative",
+    color: colors.white,
+    [theme.breakpoints.down("md")]: {
+      fontSize: "24pt",
+      width: "60%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "18pt",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+      paddingTop: theme.spacing(2),
+    },
+  },
+  body: {
+    display: "flex",
+    gap: theme.spacing(6),
+    justifyContent: "center",
+    [theme.breakpoints.down("sm")]: {
+      flexWrap: "wrap",
+      gap: theme.spacing(2),
+    },
+  },
+}));
+
+const cardStyles = makeStyles((theme) => ({
+  root: {
+    boxSizing: "border-box",
+    background: "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
+    backgroundSize: "400% 400%",
+    transition: "ease-out 0.4s",
+    "&:hover": {
+      backgroundPosition: "100% 20%",
+      transform: "translateY(-5px)",
+    },
+  },
+  header: {
+    padding: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    boxSizing: "inherit",
+    height: "50%",
+
+    fontSize: "16pt",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "12pt",
+    },
+  },
+  imageHeader: {
+    height: "80%",
+    width: "auto",
+  },
+  body: {
+    padding: theme.spacing(2),
+    height: "20%",
+    boxSizing: "inherit",
+    width: "100%",
+  },
+  footer: {
+    padding: theme.spacing(2),
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    height: "30%",
+    boxSizing: "inherit",
+    width: "100%",
+    backgroundColor: colors.gray,
   },
 }));
